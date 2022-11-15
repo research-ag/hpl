259aaa0faca7deaa7804ea8a77003d29f43e2b92
Timo Hanke <t@timohanke.org>
2022-11-15 05:53:11 +0100

    bump ic-repl to 0.3.9
    adjust relative paths in the ic-repl scripts after a bug got
    fixed in ic-repl

--- test/performance_tests/test_aggregator.sh
+++ test/performance_tests/test_aggregator.sh
@@ -19,7 +19,7 @@ identity user;
 let tx = call canister.generateSimpleTx(user, 0, anotherUser, 0, 10);
 let res0 = call canister.profileSubmit(tx);
 res0;
-output("./performance_tests/cycle_stats.txt", stringify("[AGG] submit simple Tx: ", res0[0], "\n"));
+output("cycle_stats.txt", stringify("[AGG] submit simple Tx: ", res0[0], "\n"));
 
 let tx = call canister.generateSimpleTx(user, 0, user, 1, 10);
 let res1 = call canister.profileSubmit(tx);
@@ -27,12 +27,12 @@ res1;
 // should be automatically put into batch, since approved
 let batch = call canister.getNextBatch();
 assert batch[0] != null;
-output("./performance_tests/cycle_stats.txt", stringify("[AGG] submit + auto-enqueue simple Tx: ", res1[0], "\n"));
+output("cycle_stats.txt", stringify("[AGG] submit + auto-enqueue simple Tx: ", res1[0], "\n"));
 
 let heavy_tx = call canister.generateHeavyTx(0);
 let res2 = call canister.profileSubmit(heavy_tx);
 res2;
-output("./performance_tests/cycle_stats.txt", stringify("[AGG] submit heavy tx: ", res2[0], "\n"));
+output("cycle_stats.txt", stringify("[AGG] submit heavy tx: ", res2[0], "\n"));
 
 // clear batch if any
 call canister.getNextBatch();
@@ -46,7 +46,7 @@ res3;
 // should be automatically put into batch, since approved
 let batch = call canister.getNextBatch();
 assert batch[0] != null;
-output("./performance_tests/cycle_stats.txt", stringify("[AGG] approve + equeue simple Tx: ", res3[0], "\n"));
+output("cycle_stats.txt", stringify("[AGG] approve + equeue simple Tx: ", res3[0], "\n"));
 
 identity user;
 let tx = call canister.generateSimpleTx(user, 0, anotherUser, 0, 10);
@@ -55,11 +55,11 @@ let gid = _.ok;
 identity anotherUser;
 let res5 = call canister.profileReject(gid);
 res5;
-output("./performance_tests/cycle_stats.txt", stringify("[AGG] reject simple Tx: ", res5[0], "\n"));
+output("cycle_stats.txt", stringify("[AGG] reject simple Tx: ", res5[0], "\n"));
 
 // big batch
 identity user;
 call canister.submitManySimpleTxs(1000, user, 0, user, 1, 10);
 let res6 = call canister.profileGetNextBatch();
 res6;
-output("./performance_tests/cycle_stats.txt", stringify("[AGG] prepare big batch: ", res6[0], "\n"));
+output("cycle_stats.txt", stringify("[AGG] prepare big batch: ", res6[0], "\n"));
